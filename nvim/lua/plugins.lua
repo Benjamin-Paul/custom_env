842144ecba17c958f5dc40784ce6c31bf272c375
Benooou <benjamin.olivier.paul@gmail.com>
2024-02-27 23:45:58 +0100

    remove a trailing space

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use { 
+    use {
         'catppuccin/nvim', as = 'catppuccin'
     }
     use {
